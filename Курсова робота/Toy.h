6797279c12d711444d840cfc9e203367a37469ad
Lesiuk-V <lenono526@gmail.com>
2020-06-01 12:45:26 +0300

    /

---
+++ Курсова робота/Toy.h
@@ -0,0 +1,25 @@
+#include "Seller.h"
+#pragma once
+class Toy
+{
+private:
+	int id;
+	char name[50];
+	char age—ategory[10];
+	double price;
+	Seller seller;
+	bool sold;
+public:
+    void create();
+    void showData();
+    void read(int pn);
+    void write();
+    static int count();
+    int search(int variant);
+    void deleteData();
+    void editData();
+    int setId();
+    bool getSold();
+    void getSeller();
+};
+
--- Курсова робота/Курсова робота.cpp
+++ Курсова робота/Курсова робота.cpp
@@ -1,20 +1,112 @@
-﻿// Курсова робота.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
-//
-
+﻿#include "Toy.h"
+#include "Seller.h"
 #include <iostream>
+using namespace std;
 
-int main()
-{
-    std::cout << "Hello World!\n";
+void print_menu() {
+    system("cls");
+    cout << "          Магазин іграшок\n" << endl;
+    cout << "1.  Додати продавця" << endl;
+    cout << "2.  Переглянути продавців" << endl;
+    cout << "3.  Пошук продавця" << endl;
+    cout << "4.  Редагування продаця" << endl;
+    cout << "5.  Видалення продавця" << endl << endl;
+
+    cout << "6.  Додати іграшку" << endl;
+    cout << "7.  Переглянути іграшки" << endl;
+    cout << "8.  Пошук іграшок" << endl;
+    cout << "9.  Редагування іграшки" << endl;
+    cout << "10. Видалення іграшки" << endl;
+    cout << "11. Вихід" << endl;
+    cout << ">";
 }
 
-// Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
-// Отладка программы: F5 или меню "Отладка" > "Запустить отладку"
+void printSearchSellerMenu()
+{
+    cout << "Пошук за полем: \n";
+    cout << "1. Ім'я" << endl;
+    cout << "2. Прізвище" << endl;
+    cout << "3. По батькові" << endl;
+    cout << "4. Вік" << endl;
+    cout << "5. id" << endl;
+}
+void printSearchToyMenu()
+{
+    cout << "Пошук за полем: \n";
+    cout << "1. Назва" << endl;
+    cout << "2. Вікова категорія" << endl;
+    cout << "3. Ціна" << endl;
+    cout << "4. id працівника" << endl;
+    cout << "5. Продано" << endl;
+    //cout << "6. Номером документа" << endl;
+    //cout << "7. Дійсний до" << endl;
+}
+int get_variant(int max) {
+    double input = -1;
+    bool valid = false;
+    do
+    {
+        cin >> input;
+        if (cin.good())
+        {
+            valid = true;
+            if (input > 0 && input < max)
+                return input;
+            else
+            {
+                valid = false;
+                cout << "Помилка вводу. Введіть ще раз" << endl;
+            }
 
-// Советы по началу работы 
-//   1. В окне обозревателя решений можно добавлять файлы и управлять ими.
-//   2. В окне Team Explorer можно подключиться к системе управления версиями.
-//   3. В окне "Выходные данные" можно просматривать выходные данные сборки и другие сообщения.
-//   4. В окне "Список ошибок" можно просматривать ошибки.
-//   5. Последовательно выберите пункты меню "Проект" > "Добавить новый элемент", чтобы создать файлы кода, или "Проект" > "Добавить существующий элемент", чтобы добавить в проект существующие файлы кода.
-//   6. Чтобы снова открыть этот проект позже, выберите пункты меню "Файл" > "Открыть" > "Проект" и выберите SLN-файл.
+        }
+        else
+        {
+            cin.clear();
+            cin.ignore(numeric_limits<streamsize>::max(), '\n');
+            cout << "Помилка вводу. Введіть ще раз" << endl;
+        }
+    } while (!valid);
+    return input;
+}
+int main()
+{
+    system("chcp 1251>nul");
+    int variant;
+    string str;
+    Toy toy;
+    int n;
+    Seller seller;
+    do {
+        print_menu();
+        char ch;
+        variant = get_variant(11);
+        switch (variant) {
+        case 1:
+            break;
+
+        case 2:
+            break;
+        case 3:
+            break;
+        case 4:
+            break;
+        case 5:
+            break;
+        case 6:
+            break;
+        case 7:
+            break;
+        case 8:
+            break;
+        case 9:
+            break;
+        case 10:
+            break;
+        default:
+            cout << "Введіть число від 1 до 11\n";
+        }
+        if (variant != 11 || variant == 't')
+            system("pause");
+    } while (variant != 11);
+    return 0;
+}
